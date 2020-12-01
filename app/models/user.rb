be618aef4583238d72bf6ee07e807bf8df3d2fb1
Raphaelle de La Bouillerie <rdelabouillerie@yahoo.fr>
2020-12-01 17:13:05 +0100

    adding classname to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :gardens
+  has_many :appointments, through: :gardens
+  has_many :guest_list, foreign_key: "guest_id", class_name: "Appointment"
+  has_many :host_list, foreign_key: "host_id", class_name: "Appointment"
+
 end
