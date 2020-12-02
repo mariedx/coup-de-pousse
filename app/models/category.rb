611a27a8064306e932f356b99e49843a0c6b68de
Raphaelle de La Bouillerie <rdelabouillerie@yahoo.fr>
2020-12-02 10:51:21 +0100

    category model creation

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+class Category < ApplicationRecord
+    has_many :garden_categories
+    has_many :gardens, through: :garden_categories
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-<<<<<<< HEAD
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-=======
   #lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
->>>>>>> develop
   has_many :gardens
   has_many :appointments, through: :gardens
   has_many :guest, foreign_key: "guest_id", class_name: "Appointment"
