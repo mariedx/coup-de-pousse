0d1853b94b7e1266b17fa69488c524dc51d363d8
Raphaelle de La Bouillerie <rdelabouillerie@yahoo.fr>
2020-12-02 09:46:32 +0100

    correcting foreign-keys in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :gardens
   has_many :appointments, through: :gardens
-  has_many :guest_list, foreign_key: "guest_id", class_name: "Appointment"
-  has_many :host_list, foreign_key: "host_id", class_name: "Appointment"
+  has_many :guest, foreign_key: "guest_id", class_name: "Appointment"
+  has_many :host, foreign_key: "host_id", class_name: "Appointment"
 
 end
