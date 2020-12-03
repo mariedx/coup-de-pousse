48eb2e88e2e19d097643cac514be9e4f36257b07
Clementine <clementine.fouque@gmail.com>
2020-12-03 16:32:17 +0100

    new appointment email for guests

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -1,2 +1,13 @@
 class GuestMailer < ApplicationMailer
+
+  def app_send_email (guest, appointment)
+
+    @guest = current_user
+    @appointment = appointment
+
+    mail(to: @guest.email, subject: `Votre demande de rendez-vous a été envoyé !`)
+
+
+  end
+
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,15 @@ class Appointment < ApplicationRecord
   belongs_to :host, class_name: "User"
   belongs_to :garden
 
+  after_create :new_app_guest, :new_app_host
+
+  def new_app_guest
+    GuestMailer.app_send_email(self).deliver_now
+  end
+
+  def new_app_host
+    HostMailer.app_received_email(self).deliver_now
+  end
+
+
 end
