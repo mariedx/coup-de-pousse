851e7ba1bf3c827e05841b9379d9c1f0e8480442
Clementine <clementine.fouque@gmail.com>
2020-12-03 23:17:22 +0100

    fix conflict views

--- db/schema.rb
+++ db/schema.rb
@@ -100,8 +100,10 @@ ActiveRecord::Schema.define(version: 2020_12_02_173402) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
+    t.string "description", default: ""
+    t.string "tools", default: ""
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
