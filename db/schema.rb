3963cd83d7c2aa20fdf7fa2ca5348b9687cfa3ec
BenjaminServal <benjiserval@gmail.com>
2020-12-03 10:02:25 +0100

    show profil add descriptions and tools

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(version: 2020_12_02_095632) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
+    t.string "description", default: ""
+    t.string "tools", default: ""
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
