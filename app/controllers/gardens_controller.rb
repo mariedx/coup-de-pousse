26fceeeca0e0f0774e8d0cb6a413079ad9345307
MarieDechaux <marie.dechaux@gmail.com>
2020-12-03 16:55:35 +0100

    garden show with form contact

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -2,4 +2,9 @@ class GardensController < ApplicationController
   def index
     @gardens = Garden.all
   end
+
+  def show
+    @garden = Garden.find(params[:id])
+    @user = @garden.user
+  end
 end
