36c48bdbdabdfc3f5e4e4ced69439b93905ada62
Aimery Cayol <aimerycayol@gmail.com>
2020-12-03 20:07:06 +0100

    add first form

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,36 @@
 class GardensController < ApplicationController
+
+  def new 
+    @garden = Garden.new
+  end 
+
+  def create
+    @user.id = current_user.id
+    @garden = Garden.create(gardens_params)
+
+      if @garden.save 
+         redirect_to root_path
+         flash[:success] = "Annonce créée avec succès !"
+
+      else
+        render :new
+      end
+  end
+
+  def show
+    @garden = Garden.find(params[:id])
+  end
+
   def index
     @gardens = Garden.all
   end
+
+
+  private
+  def gardens_params
+    gardens_params = params.require(:garden).permit(:title, :description, :orientation, :floor_type, :is_available, :parking, :tools_available, :surface, :image_url)
+  end
+
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
 
-  def to_param
-    first_name
-  end
+  
 
 end
