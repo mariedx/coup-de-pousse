ab84c0195df6f630fb82b9e2a06adb6ba78e9cc5
Raphaelle de La Bouillerie <rdelabouillerie@yahoo.fr>
2020-12-04 18:43:03 +0100

    last modification on routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,4 @@ Rails.application.routes.draw do
     resources :appointments, only: [:new, :create, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+end
