abe54c67ad8500bf93331c59a38cb370973e9100
Clementine <clementine.fouque@gmail.com>
2020-12-07 11:51:13 +0100

    add starts in future and start_must_be_before_end_date in model appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,19 @@ class Appointment < ApplicationRecord
   belongs_to :host, class_name: "User"
   belongs_to :garden
 
+  validates_presence_of :start_date, :end_date
+  validate :start_must_be_before_end_date
+  validate :start_must_be_futur
+
+  private
+  
+  def start_must_be_before_end_date
+      errors.add(:start_date, "doit être avant la date de fin") unless
+        self.start_date < self.end_date
+  end
+
+  def start_in_future
+    errors.add(:start_date, ": Impossible de réserver un rendez-vous dans le passé") unless start_date > DateTime.now
+  end
+
 end
