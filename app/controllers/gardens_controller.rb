ee4752e09eedbb5e26424d50ddbc653d781602f8
MarieDechaux <marie.dechaux@gmail.com>
2020-12-08 15:45:06 +0100

    dashboard updated with tabs

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -7,7 +7,6 @@ class GardensController < ApplicationController
   def show
     @garden = Garden.find(params[:id])
     @user = @garden.user
-
   end
 
   def edit 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,27 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-    def show
-      @user = User.find(current_user.id)
+  def show
+    @user = User.find(params[:id])
 
-  #showing user's favorites
-      @user_favorites = current_user.favorites
-      @favorited_gardens = []
-      @user_favorites.each do |fav|
-        @favorited_gardens << fav.garden
-      end
+    #showing user's favorites
+    @user_favorites = current_user.favorites
+    @favorited_gardens = [] 
+    @user_favorites.each do |fav|
+    @favorited_gardens << fav.garden
+    end
 
+    #selecting garden created by user
+    @gardens = @user.gardens
 
-  #selecting garden created by user
-      @gardens = @user.gardens
-
-  #finding the user's appointments
-      @appointments = Appointment.all
-      @appointments.each do |app|
-        if (app.host || app.guest) == current_user
-        @appointments << app
-        end
+    #finding the user's appointments
+    @appointments = Appointment.all
+    @appointments.each do |app|
+      if (app.host || app.guest) == current_user
+      @appointments << app
       end
     end
+  end
 
   def create
     flash[:notice] = "Registration needed"
@@ -44,6 +43,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :avatar, :description, :tools)
   end
