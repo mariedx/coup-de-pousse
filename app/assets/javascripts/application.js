0db3231400c9f9344813a4264ea40f3156f81be4
Clementine <clementine.fouque@gmail.com>
2020-12-09 22:11:08 +0100

    attempt at custom nav_bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require activestorage
 //= require turbolinks
 //= require cable
+//= require nav
---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,9 @@
+// <!-- Function used to shrink nav bar removing paddings and adding black background -->
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.nav').addClass('affix');
+      console.log("OK");
+    } else {
+      $('.nav').removeClass('affix');
+    }
+  });
